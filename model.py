df02fa225b1c65a7f2e9b56e959a0abba13ba060
shaswat128 <shaswatpanda128@gmail.com>
2026-04-07 11:00:45 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
+    grade = models.IntegerField(default=1)
 
     def __str__(self):
         return self.text
@@ -16,10 +25,18 @@ class Choice(models.Model):
         return self.text
 
 
+class Enrollment(models.Model):
+    student_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.student_name
+
+
 class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    selected_choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
     submitted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.question} - {self.selected_choice}"
+        return f"{self.enrollment} - {self.question}"
